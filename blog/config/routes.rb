64675acb187fcaf2a509515be7918d1bfd01c6e4
Tim <tingyuhe@hotmail.com>
2015-04-03 13:15:50 -0700

    Add new stuff

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root :to => "articles#index"
-  resources :articles,
-            :users
+  resources :articles do
+    resources :comments
+  end
+  resources :users
+  resources :session
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
