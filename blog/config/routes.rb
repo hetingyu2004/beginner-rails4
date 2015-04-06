04412044a847ac5a201cd48a0c0ce808cde7815c
Tim <tingyuhe@hotmail.com>
2015-04-06 00:27:50 -0700

    yolo

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
-  resources :session
+  resource :session, :only => [:new, :create, :destroy]
+  get '/login' => 'sessions#new', :as => 'login'
+  get '/logout' => 'sessions#destroy', :as => 'logout'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
